fae2c0a8ebe448da37c4d2740bba99bd48a204c0
Tuyen Le <tuyenle1099@gmail.com>
2021-03-31 16:33:12 +0700

    add SalesSystem-Project

---
+++ SalesSystem-Project/INSERT.sql
@@ -0,0 +1,248 @@
+﻿USE PTTK_TH
+ GO
+--INSERT
+--INSERT KHACHHANG
+INSERT INTO dbo.KHACHHANG VALUES (100, N'Lydia Lloyd', N'Nữ', N'0323481494', N'oxlvrmwc.qmzeyxjqm@gmail.com', N'67 Clarendon Boulevard')
+INSERT INTO dbo.KHACHHANG VALUES (101, N'Chester Glover', N'Nam', N'0985535761', N'mtalyc2@gmail.com', N'168 Hague St.')
+INSERT INTO dbo.KHACHHANG VALUES (102, N'Danny Phillips', N'Nữ', N'0966333855', N'eptykl.jhnjdbnx@gmail.com', N'89 Second Road')
+INSERT INTO dbo.KHACHHANG VALUES (103, N'Austin Chavez', N'Nữ', N'0827646228', N'pzqdp.ruxa@gmail.com', N'63 Oak Avenue')
+INSERT INTO dbo.KHACHHANG VALUES (104, N'Kendall Ingram', N'Nữ', N'0371566658', N'oacv@gmail.com', N'21 Rocky Fabien Drive')
+INSERT INTO dbo.KHACHHANG VALUES (105, N'Valerie Dickerson', N'Nữ', N'0384747691', N'tbtnbbge.pyxalwtqho@gmail.com', N'802 Clarendon Boulevard')
+INSERT INTO dbo.KHACHHANG VALUES (106, N'Lisa Aguilar', N'Nữ', N'0385456191', N'vgpnvkvg7@gmail.com', N'44 Hague Road')
+INSERT INTO dbo.KHACHHANG VALUES (107, N'Vicki Payne', N'Nữ', N'0368615527', N'nepvmobi@gmail.com', N'20 Green Nobel Freeway')
+INSERT INTO dbo.KHACHHANG VALUES (108, N'Elias Garrett', N'Nữ', N'0346941892', N'ezdwde049@gmail.com', N'285 Old St.')
+INSERT INTO dbo.KHACHHANG VALUES (109, N'Allison Hoffman', N'Nữ', N'0331334371  ', N'vdzg.xnqvxjw@gmail.com', N'334 Fabien Way')
+INSERT INTO dbo.KHACHHANG VALUES (110, N'Betsy Mc Lean', N'Nữ', N'0874221186  ', N'ahbg8@gmail.com', N'96 Hague Blvd.')
+
+--INSERT DOITAC--
+insert into DOITAC values (100, N'Trang VN EXPRESS','01/03/2019','01/03/2020', N'Trên cùng website')
+insert into DOITAC values (101, N'Trang kênh14', '01/04/2020','01/04/2021', N'Giữa website')
+insert into DOITAC values (102, N'Trang Zing News', '01/05/2020','01/05/2022', N'Trên cùng website')
+insert into DOITAC values (103, N'Trang Tin Tức 24h', '01/05/2019','01/05/2021', N'Trên cùng website')
+insert into DOITAC values (104, N'Trang Vietnamnet', '01/07/2020','01/07/2025', N'Góc trên bên phải')
+insert into DOITAC values (105, N'Trang Báo Tuổi Trẻ', '01/03/2018','01/03/2019', N'Dọc hai bên nội dung chính website')
+insert into DOITAC values (106, N'Trang Đại Kỷ Nguyên', '01/05/2019','01/09/2022', N'Dọc hai bên nội dung chính website')
+insert into DOITAC values (107, N'Trang Đời Sống Pháp Luật', '01/07/2019','01/07/2020', N'Trên cùng website')
+insert into DOITAC values (108, N'Trang Đại Kỷ Nguyên', '01/05/2019','01/09/2022', N'Dọc hai bên nội dung chính website')
+insert into DOITAC values (109, N'Trang Dân Trí', '01/01/2017','01/01/2020', N'Giữa website')
+insert into DOITAC values (110, N'Trang Thanh Niên', '01/05/2020','01/09/2023', N'Góc trên bên phải')
+
+--INSERT NHACUNGCAP
+INSERT INTO NHACUNGCAP VALUES (100,N'Hương Việt',N'0337961984',N'102 Trường Chinh');
+INSERT INTO NHACUNGCAP VALUES (101,N'Away',N'034567892',N'502 Nguyễn Tất Thành');
+INSERT INTO NHACUNGCAP VALUES (102,N'Lung Linh',N'09768512',N'226 Hai Bà Trưng');
+INSERT INTO NHACUNGCAP VALUES (103,N'Tuổi Thơ',N'08765489',N'87 Hải Thượng Lãn Ông');
+INSERT INTO NHACUNGCAP VALUES (104,N'Nam Ngư',N'03789415',N'91 Phan Văn Trị');
+INSERT INTO NHACUNGCAP VALUES (105,N'Hồng Hạc',N'033758741',N'23 Nguyễn Thị Nghỉ');
+INSERT INTO NHACUNGCAP VALUES (106,N'Onlay',N'015879463',N'58 Đường Cách Mạng Tháng Tám');
+INSERT INTO NHACUNGCAP VALUES (107,N'Phay',N'05846971',N'67 Sư Vạn Hạnh');
+INSERT INTO NHACUNGCAP VALUES (108,N'An Co',N'028791643',N'344 Phan Bội Châu');
+INSERT INTO NHACUNGCAP VALUES (109,N'Bông',N'012587596',N'89 Hùng Vương');
+INSERT INTO NHACUNGCAP VALUES (110,N'Enco',N'025897561',N'54 An Dương Vương');
+
+--INSERT NHANVIEN
+insert into NHANVIEN values (100, N'Lý Hoàng Hà', '10/23/1984', N'Nam', N'22/5 Nguyễn Xí, Q.Bình Thạnh, TP HCM', N'Nhân viên bán hàng')
+insert into NHANVIEN values (101, N'Trương Nam Sơn', '06/20/1989', N'Nam', N'215 Lý Thường Kiệt,TP Biên Hòa', N'Nhân viên quản lý')
+insert into NHANVIEN values (102 , N'Nguyễn Ngọc Ánh', '05/11/1985', N'Nữ', N'12/21	Võ Văn Ngân	Thủ Đức, TP HCM', N'Nhân viên bán hàng')
+insert into NHANVIEN values (103 , N'Lê Ngọc Anh', '08/19/1991', N'Nữ', N'201 Hùng Vương, Q.5, TP HCM', N'Nhân viên bán hàng')
+insert into NHANVIEN values (104 , N'Nguyễn An Trung', '06/05/1986', N'Nam', N'234 3/2, TP Biên Hòa', N'Nhân viên đăng tin')
+insert into NHANVIEN values (105 , N'Trần Trung Hiếu', '08/06/1987', N'Nam', N'22/11 Lý Thường Kiệt, TP Mỹ Tho', N'Nhân viên giao hàng')
+insert into NHANVIEN values (106 , N'Trần Bạch Tuyết', '05/20/1990', N'Nữ', N'127 Hùng Vương, TP Mỹ Tho', N'Thủ quỹ')
+insert into NHANVIEN values (107 , N'Trần Trà Hương', '06/20/1981', N'Nữ',N'125	Trần Hưng Đạo, Q.1,TP HCM', N'Nhân viên quản lý')
+insert into NHANVIEN values (108 , N'Nguyễn Hoài An', '02/15/1993', N'Nam', N'25/3 Lạc Long Quân, Q.10, TP HCM', N'Nhân viên bán hàng')
+insert into NHANVIEN values (109 , N'Lý Hoàng Hà', '10/23/1984', N'Nam', N'22/5	Nguyễn Xí, Q.Bình Thạnh, TP HCM', N'Nhân viên giao hàng')
+insert into NHANVIEN values (110 , N'Phạm Nam Thanh', '12/12/1992', N'Nam', N'221 Hùng Vương, Q.5, TP HCM', N'Nhân viên giao hàng')
+
+--INSERT SANPHAM
+INSERT INTO dbo.SANPHAM
+        ( maSP, ten, loai, gia )
+VALUES  ( 100,N'Tivi LG 50inch', N'Hàng điện tử', 10990000 )
+INSERT INTO dbo.SANPHAM
+        ( maSP, ten, loai, gia )
+VALUES  ( 101, N'Nồi chiên không dầu Lock&Lock', N'Hàng gia dụng', 3500000 )
+INSERT INTO dbo.SANPHAM
+        ( maSP, ten, loai, gia )
+VALUES  ( 102, N'Lò vi sóng Sunhouse', N'Hàng gia dụng', 2400000 )
+INSERT INTO dbo.SANPHAM
+        ( maSP, ten, loai, gia )
+VALUES  ( 103, N'Chảo chống dính', N'Hàng gia dụng', 150000 )
+INSERT INTO dbo.SANPHAM
+        ( maSP, ten, loai, gia )
+VALUES  ( 104, N'Máy giặt LG', N'Hàng điện tử', 11500000 )
+INSERT INTO dbo.SANPHAM
+        ( maSP, ten, loai, gia )
+VALUES  ( 105, N'Máy giặt Toshiba', N'Hàng điện tử', 6279000 )
+INSERT INTO dbo.SANPHAM
+        ( maSP, ten, loai, gia )
+VALUES  ( 106, N'Tủ lạnh Sharp Inverter 165L', N'Hàng điện tử', 4290000 )
+INSERT INTO dbo.SANPHAM
+        ( maSP, ten, loai, gia )
+VALUES  ( 107, N'Tủ lạnh Aqua 130L', N'Hàng điện tử', 3590000 )
+INSERT INTO dbo.SANPHAM
+        ( maSP, ten, loai, gia )
+VALUES  ( 108, N'Nồi cơm điện tử Toshiba', N'Hàng gia dụng', 2069000 )
+INSERT INTO dbo.SANPHAM
+        ( maSP, ten, loai, gia )
+VALUES  ( 109, N'Nồi cơm điện nắp rời Sharp', N'Hàng gia dụng', 499000 )
+INSERT INTO dbo.SANPHAM
+        ( maSP, ten, loai, gia )
+VALUES  ( 110, N'Nồi chiên không dầu Philips', N'Hàng gia dụng', 2699000 )
+
+--INSERT DONNHAPHANG:
+INSERT INTO DONNHAPHANG VALUES (100, 20, '11/04/2020', 50000000, 100, 101, 100);
+INSERT INTO DONNHAPHANG VALUES (101, 10, '09/03/2020', 600000000, 101, 103, 101);
+INSERT INTO DONNHAPHANG VALUES (102, 15, '02/05/2020', 508000000, 104, 104, 106);
+INSERT INTO DONNHAPHANG VALUES (103, 5, '08/01/2020', 78000000, 104, 109, 105);
+INSERT INTO DONNHAPHANG VALUES (104, 16, '06/06/2020', 84200000, 109, 107, 108);
+INSERT INTO DONNHAPHANG VALUES (105, 9, '01/02/2020', 214500000, 101, 104, 102);
+INSERT INTO DONNHAPHANG VALUES (106, 25, '01/07/2020', 78400000, 110, 103, 110);
+INSERT INTO DONNHAPHANG VALUES (107, 10, '02/01/2020', 90000000, 103, 102, 109);
+INSERT INTO DONNHAPHANG VALUES (108, 15, '04/07/2020', 780000000, 107, 110, 107);
+INSERT INTO DONNHAPHANG VALUES (109, 10, '06/01/2020', 140000000, 106, 108, 103);
+INSERT INTO DONNHAPHANG VALUES (110, 5, '04/02/2020', 10000000, 100, 105, 104);
+
+--INSERT CT_DONNHAPHANG:
+INSERT INTO CT_DONNHAPHANG VALUES (100, 102, 500000, 47580, 50000000);
+INSERT INTO CT_DONNHAPHANG VALUES (101, 110, 875600, 25000, 600000000);
+INSERT INTO CT_DONNHAPHANG VALUES (102, 103, 845000, 25000, 508000000);
+INSERT INTO CT_DONNHAPHANG VALUES (103, 109, 56000, 80000, 78000000);
+INSERT INTO CT_DONNHAPHANG VALUES (104, 105, 40000, 5400, 84200000);
+INSERT INTO CT_DONNHAPHANG VALUES (105, 107, 120000, 2000, 214500000);
+INSERT INTO CT_DONNHAPHANG VALUES (106, 108, 145000, 5000, 78400000);
+INSERT INTO CT_DONNHAPHANG VALUES (107, 104, 170000, 6000, 90000000);
+INSERT INTO CT_DONNHAPHANG VALUES (108, 100, 56000, 8500, 780000000);
+INSERT INTO CT_DONNHAPHANG VALUES (109, 106, 8400, 4000, 140000000);
+INSERT INTO CT_DONNHAPHANG VALUES (110, 101, 9500, 1200, 10000000);
+
+--INSERT DONTRAHANG:
+INSERT INTO DONTRAHANG VALUES (100, 20, '12/04/2020', 890000000, 102, 100);
+INSERT INTO DONTRAHANG VALUES (101, 5, '08/04/2020', 60000000, 101, 104);
+INSERT INTO DONTRAHANG VALUES (102, 15, '01/06/2020', 5000000, 104, 105);
+INSERT INTO DONTRAHANG VALUES (103, 5, '10/05/2020', 8000000, 105, 102);
+INSERT INTO DONTRAHANG VALUES (104, 10, '07/06/2020', 8200000, 109, 101);
+INSERT INTO DONTRAHANG VALUES (105, 10, '02/02/2020', 2500000, 107, 110);
+INSERT INTO DONTRAHANG VALUES (106, 20, '12/07/2020', 7400000, 106, 108);
+INSERT INTO DONTRAHANG VALUES (107, 5, '12/03/2020', 60000000, 110, 107);
+INSERT INTO DONTRAHANG VALUES (108, 15, '08/07/2020', 89000000, 108, 109);
+INSERT INTO DONTRAHANG VALUES (109, 10, '05/01/2020', 15000000, 103, 106);
+INSERT INTO DONTRAHANG VALUES (110, 5, '09/04/2020', 1000000, 100, 103);
+
+--INSERT CT_DONTRAHANG:
+INSERT INTO CT_DONTRAHANG VALUES (100, 102, 500000, 500, 890000000);
+INSERT INTO CT_DONTRAHANG VALUES (101, 103, 854000, 300, 60000000);
+INSERT INTO CT_DONTRAHANG VALUES (102, 109, 56000, 900, 5000000);
+INSERT INTO CT_DONTRAHANG VALUES (103, 108, 14500, 200, 8000000);
+INSERT INTO CT_DONTRAHANG VALUES (104, 100, 56000, 600, 18200000);
+INSERT INTO CT_DONTRAHANG VALUES (105, 101, 9500, 250, 2500000);
+INSERT INTO CT_DONTRAHANG VALUES (106, 105, 40000, 900, 7400000);
+INSERT INTO CT_DONTRAHANG VALUES (107, 106, 8400, 300, 60000000);
+INSERT INTO CT_DONTRAHANG VALUES (108, 107, 120000, 200, 89000000);
+INSERT INTO CT_DONTRAHANG VALUES (109, 110, 854600, 100, 15000000);
+INSERT INTO CT_DONTRAHANG VALUES (110, 104, 170000, 100, 1000000);
+
+--INSERT HOADON
+INSERT INTO dbo.HOADON ( maHD ,maKH ,trangThai ,tongCong ,ngayLap ,maNVGiaoHang ,maNVBanHang,trangThaiThanhToan,hinhThucThanhToan)
+VALUES  ( 100 ,100,N'Đơn hàng đã được tiếp nhận',13359000,'08/20/2020',105,102,N'Thanh toán thành công',N'Thanh toán thẻ')
+INSERT INTO dbo.HOADON ( maHD ,maKH ,trangThai ,tongCong ,ngayLap ,maNVGiaoHang ,maNVBanHang,trangThaiThanhToan,hinhThucThanhToan)
+VALUES  ( 101 ,103,N'Đơn hàng đã được tiếp nhận',3590000,'08/15/2020',109,100,N'Thanh toán thất bại',N'Thanh toán thẻ')
+INSERT INTO dbo.HOADON ( maHD ,maKH ,trangThai ,tongCong ,ngayLap ,maNVGiaoHang ,maNVBanHang,trangThaiThanhToan,hinhThucThanhToan)
+VALUES  ( 102 ,100,N'Đang giao hàng',5087000,'08/19/2020', NULL , NULL,N'Thanh toán thành công',N'Thanh toán thẻ')
+INSERT INTO dbo.HOADON ( maHD ,maKH ,trangThai ,tongCong ,ngayLap ,maNVGiaoHang ,maNVBanHang,trangThaiThanhToan,hinhThucThanhToan)
+VALUES  ( 103 ,100,N'Giao hàng thành công',12486000,'08/12/2020',110,100,N'Thanh toán thành công',N'Thanh toán thẻ')
+INSERT INTO dbo.HOADON ( maHD ,maKH ,trangThai ,tongCong ,ngayLap ,maNVGiaoHang ,maNVBanHang,trangThaiThanhToan,hinhThucThanhToan)
+VALUES  ( 104 ,110,N'Đang giao hàng',10990000,'08/18/2020',109,102,N'Thanh toán thành công',N'Thanh toán thẻ')
+INSERT INTO dbo.HOADON ( maHD ,maKH ,trangThai ,tongCong ,ngayLap ,maNVGiaoHang ,maNVBanHang,trangThaiThanhToan,hinhThucThanhToan)
+VALUES  ( 105 ,103,N'Giao hàng thành công',2400000,'08/20/2020',105,103,N'Thanh toán thành công',N'Thanh toán tiền mặt')
+INSERT INTO dbo.HOADON ( maHD ,maKH ,trangThai ,tongCong ,ngayLap ,maNVGiaoHang ,maNVBanHang,trangThaiThanhToan,hinhThucThanhToan)
+VALUES  ( 106 ,102,N'Đang giao hàng',2400000,'08/20/2020',105,103,N'Chưa thanh toán',N'Thanh toán tiền mặt')
+
+--INSERT CHITIETDONHANG
+INSERT INTO dbo.CHITIEDONHANG (maSP ,maHoaDon ,soLuong ,tongCong ,soLuongTra ,ngayTra)
+VALUES  ( 100 , 100 , 1 , 10990000 , 0 , NULL)
+
+INSERT INTO dbo.CHITIEDONHANG (maSP ,maHoaDon ,soLuong ,tongCong ,soLuongTra ,ngayTra)
+VALUES  ( 103 , 100 , 2 , 300000 , 0 ,NULL )
+
+INSERT INTO dbo.CHITIEDONHANG (maSP ,maHoaDon ,soLuong ,tongCong ,soLuongTra ,ngayTra)
+VALUES  ( 108 , 100 , 1 , 2069000 , 0 , NULL)
+
+INSERT INTO dbo.CHITIEDONHANG (maSP ,maHoaDon ,soLuong ,tongCong ,soLuongTra ,ngayTra)
+VALUES  ( 107 , 101 , 1 , 3590000 , 0 , NULL)
+
+INSERT INTO dbo.CHITIEDONHANG (maSP ,maHoaDon ,soLuong ,tongCong ,soLuongTra ,ngayTra)
+VALUES  ( 106 , 102 , 1 , 3590000 , 0 , NULL)
+
+INSERT INTO dbo.CHITIEDONHANG (maSP ,maHoaDon ,soLuong ,tongCong ,soLuongTra ,ngayTra)
+VALUES  ( 109 , 102 , 3 ,1497000 , 0 ,NULL )
+
+INSERT INTO dbo.CHITIEDONHANG (maSP ,maHoaDon ,soLuong ,tongCong ,soLuongTra ,ngayTra)
+VALUES  ( 108 , 103 , 4 , 6207000 , 1 , '08/18/2020')
+
+INSERT INTO dbo.CHITIEDONHANG (maSP ,maHoaDon ,soLuong ,tongCong ,soLuongTra ,ngayTra)
+VALUES  ( 105 , 103 , 1 , 6279000 , 0 , NULL)
+
+INSERT INTO dbo.CHITIEDONHANG (maSP ,maHoaDon ,soLuong ,tongCong ,soLuongTra ,ngayTra)
+VALUES  ( 100 , 104 , 1 , 10990000 , 0 , NULL)
+
+INSERT INTO dbo.CHITIEDONHANG (maSP ,maHoaDon ,soLuong ,tongCong ,soLuongTra ,ngayTra)
+VALUES  ( 102 , 105 , 1 , 2400000 , 0 , NULL)
+
+
+--INSERT HOADONTHANHTOANTHE
+INSERT INTO dbo.HOADONTHANHTOANTHE VALUES (100, 1035260946014270, N'Đã xác nhận')
+INSERT INTO dbo.HOADONTHANHTOANTHE VALUES (101, 1402483640873380, N'Đã xác nhận')
+INSERT INTO dbo.HOADONTHANHTOANTHE VALUES (102, 5743524093700280, N'Chưa xác nhận')
+INSERT INTO dbo.HOADONTHANHTOANTHE VALUES (103, 6294661763507700, N'Đã xác nhận')
+INSERT INTO dbo.HOADONTHANHTOANTHE VALUES (104, 5541087022896910, N'Đã xác nhận')
+
+--INSERT COMMENT
+insert into COMMENT values (100, 107, 105, '07/07/2020', N'Bình thường', 3)
+insert into COMMENT values (101, 104, 100, '07/08/2020', N'Hài lòng', 4)
+insert into COMMENT values (102, 100, 107, '08/08/2020', N'Hơi thất vọng', 2)
+insert into COMMENT values (103, 103, 105, '08/09/2020', N'Chất lượng kém, không tốt', 1)
+insert into COMMENT values (104, 100, 105, '08/10/2020', N'Cực kì hài lòng', 5)
+insert into COMMENT values (105, 103, 110, '08/10/2020', N'Hài lòng', 5)
+insert into COMMENT values (106, 100, 107, '08/13/2020', N'Chất lượng khá tốt', 3)
+insert into COMMENT values (107, 101, 105, '08/14/2020', N'Tốt!', 5)
+insert into COMMENT values (108, 108, 103, '08/15/2020', N'Không tốt', 2)
+insert into COMMENT values (109, 101, 106, '08/17/2020', N'Được', 4)
+insert into COMMENT values (110, 109, 104, '08/21/2020', N'Tệ', 1)
+
+--INSERT THONGKECOMMENT
+insert into THONGKECOMMENT values (100, 100,'07/12/2020', N'Tốt')
+insert into THONGKECOMMENT values (100, 101, '07/12/2020', N'Tốt')
+insert into THONGKECOMMENT values (103, 102,'08/09/2020', N'Không tốt')
+insert into THONGKECOMMENT values (103, 103, '08/09/2020', N'Không tốt')
+insert into THONGKECOMMENT values (102, 104, '08/16/2020', N'Tốt')
+insert into THONGKECOMMENT values (102, 105, '08/16/2020', N'Tốt')
+insert into THONGKECOMMENT values (102, 106, '08/16/2020', N'Tốt')
+insert into THONGKECOMMENT values (102, 107, '08/16/2020', N'Tốt')
+insert into THONGKECOMMENT values (102, 108, '08/16/2020', N'Không tốt')
+
+--INSERT DANHSACHTTT
+insert into DANHSACHTTT values (100,101,'01/04/2020','01/04/2020','01/04/2021')
+insert into DANHSACHTTT values (101,102,'01/05/2020','01/05/2020','01/05/2022')
+insert into DANHSACHTTT values (103,104,'01/07/2020','01/07/2020','01/07/2025')
+insert into DANHSACHTTT values (103,108,'01/05/2019','01/05/2019','01/09/2022')
+insert into DANHSACHTTT values (105,106,'01/05/2019','01/05/2019','01/09/2022')
+insert into DANHSACHTTT values (105,105,'01/03/2018','01/03/2018','01/03/2019')
+insert into DANHSACHTTT values (103,103,'01/05/2019','01/05/2019','01/05/2021')
+insert into DANHSACHTTT values (108,108,'01/05/2019','01/05/2019','01/09/2022')
+insert into DANHSACHTTT values (108,110,'01/05/2020','01/05/2020','01/09/2023')
+insert into DANHSACHTTT values (102,102,'01/05/2020','01/05/2020','01/05/2022')
+
+--INSERT DANHSACHPTN
+insert into DANHSACHPTN values (100,101,0,'08/03/2020')
+insert into DANHSACHPTN values (100,102,1,'08/03/2020')
+insert into DANHSACHPTN values (100,103,1,'08/03/2020')
+insert into DANHSACHPTN values (101,105,1,'01/08/2020')
+insert into DANHSACHPTN values (101,106,0,'01/08/2020')
+insert into DANHSACHPTN values (101,107,1,'01/08/2020')
+insert into DANHSACHPTN values (105,108,0,'01/01/2020')
+insert into DANHSACHPTN values (105,107,0,'01/01/2020')
+insert into DANHSACHPTN values (105,106,1,'01/01/2020')
+insert into DANHSACHPTN values (105,105,1,'01/01/2020')
+
+--INSERT TAIKHOAN
+INSERT INTO TAIKHOAN VALUES (100,'thanhtuyen1007','1234','KH')
+INSERT INTO TAIKHOAN VALUES (NULL,'admin','1234','Admin')
---
+++ SalesSystem-Project/QUERY WINFORM.sql
@@ -0,0 +1,395 @@
+﻿USE PTTK_TH
+GO
+CREATE PROC USP_LOAITK @username VARCHAR(30)
+AS
+BEGIN
+SELECT username FROM TAIKHOAN WHERE @username = username AND loaiTaiKhoan = 'KH'
+END 
+GO
+
+
+--KIỂM TRA ĐĂNG NHẬP 
+CREATE PROC USP_Login @username VARCHAR(30), @password char(30)
+AS
+BEGIN
+SELECT username, matKhau FROM TAIKHOAN WHERE @username = username AND  @password = matKhau
+END 
+GO
+
+--TÌM KIẾM SẢN PHẨM
+CREATE PROC USP_TIMKIEM @tenSP NVARCHAR(50)
+AS
+BEGIN
+	SELECT maSP, ten
+	FROM SANPHAM
+	WHERE ten LIKE '%'+@tenSP+'%'
+END
+GO
+
+--THÊM SẢN PHẨM VÀO GIỎ HÀNG
+CREATE PROC USP_ThemGioHang @userName varchar(30), @maSP INT , @sl INT
+AS
+BEGIN
+	DECLARE @maKH INT,@donGia FLOAT,@tongTien FLOAT
+	SET @maKH= (SELECT MAKH FROM TAIKHOAN WHERE userName = @userName )
+	SET @donGia= (SELECT GIA FROM SANPHAM WHERE maSP = @maSP) 
+		DECLARE @TEMP INT
+		SET @TEMP = (SELECT COUNT(*) FROM GIOHANG WHERE MAKH=@maKH AND MASP=@maSP)
+		IF(@TEMP = 0)
+			BEGIN
+				SET @tongTien = @donGia*@sl
+				INSERT GIOHANG VALUES (@maKH,@maSP,@sl,@tongTien)
+				--EXEC Update_thanhTienSPGH @maKH, @maSP
+			END
+END
+GO
+
+--TÍNH TỔNG TIỀN GIỎ HÀNG CỦA 1 KHÁCH HÀNG
+CREATE PROC USP_TongTienGioHang @userName VARCHAR(30)
+	AS
+	BEGIN
+		SELECT SUM(TONGTIEN)
+		FROM GIOHANG G,TAIKHOAN T
+		WHERE @userName=T.username and G.MAKH=T.MAKH
+	END
+GO
+
+
+
+--LOAD GIỎ HÀNG
+CREATE PROC USP_LoadGioHang @userName VARCHAR(30)
+	AS
+	BEGIN
+		SELECT S.MASP,S.TEN, G.SOLUONG, S.GIA, G.TONGTIEN
+		FROM GIOHANG G,TAIKHOAN T, SANPHAM S
+		WHERE @userName=T.username AND G.MAKH=T.MAKH AND G.MASP=S.MASP
+	END
+GO
+
+--XÓA SẢN PHẨM KHỎI GIỎ HÀNG
+CREATE PROC USP_XoaGioHang @userName VARCHAR(30), @maSP INT
+	AS
+	BEGIN
+		DECLARE @maKH INT
+		SET @maKH= (SELECT MAKH FROM TAIKHOAN WHERE @userName = username)
+		DELETE GIOHANG WHERE @maSP=MASP AND @maKH=MAKH
+	END
+	GO
+
+CREATE PROC Update_thanhTien @maKH INT , @maSP INT, @soLuong INT
+AS
+BEGIN
+	DECLARE @donGia FLOAT,
+			@tongTien FLOAT
+	SET @donGia = (SELECT GIA FROM dbo.SANPHAM WHERE maSP=@maSP)
+	SET @tongTien = @donGia * @soLuong
+	UPDATE dbo.GIOHANG SET tongTien=@tongTien WHERE @maKH=maKH AND @maSP=maSP
+END
+GO
+--SỬA SỐ LƯỢNG CỦA SẢN PHẨM TRONG GIỎ HÀNG
+CREATE PROC USP_SuaGioHang @userName VARCHAR(30), @maSP INT, @soluong INT
+AS
+	BEGIN
+		DECLARE @maKH INT
+		SET @maKH= (SELECT MAKH FROM TAIKHOAN WHERE @userName =username)
+		UPDATE dbo.GIOHANG SET SOLUONG=@soluong WHERE @maSP=MASP AND @maKH=MAKH
+		EXEC Update_thanhTien @maKH, @maSP,@soLuong
+	END
+	GO
+
+--XUẤT SỔ ĐỊA CHỈ
+CREATE PROC USP_SODIACHI @userName VARCHAR(30)
+AS
+BEGIN
+	SELECT * FROM SODIACHI DC, TAIKHOAN TK
+	WHERE DC.MAKH=TK.MAKH AND TK.username=@userName
+END
+GO
+
+--THÊM ĐỊA CHỈ
+CREATE PROC USP_THEMDIACHI 
+	@userName VARCHAR(30),
+	@hoTEN NVARCHAR(50),
+	@SDT CHAR(10),
+	@tinh NVARCHAR(30),
+	@quan	NVARCHAR(30),
+	@phuong NVARCHAR(30),
+	@diaChi NVARCHAR(50)
+AS
+BEGIN
+	DECLARE @maKH INT,
+			@STT INT
+	
+	SET @maKH= (SELECT MAKH FROM TAIKHOAN WHERE @userName =username)
+	SET @STT = (SELECT MAX(STT) FROM dbo.SODIACHI WHERE @maKH = maKH)
+	IF(@STT IS NULL)
+		BEGIN
+			SET @STT = 1
+		END
+	ELSE
+		BEGIN
+			SET @STT = @STT + 1
+		END
+	INSERT SODIACHI VALUES(@maKH,@STT,@hoTEN,@SDT,@diaChi,@phuong,@quan,@tinh)
+END
+GO
+ 
+
+--LOAD SỔ ĐỊA CHỈ
+CREATE PROC USP_LoadSoDiaChi @userName VARCHAR(30)
+	AS
+	BEGIN
+	DECLARE @maKH INT
+		SET @maKH= (SELECT MAKH FROM TAIKHOAN WHERE @userName =username)
+		SELECT * FROM dbo.SODIACHI WHERE @maKH = maKH
+	END
+GO
+
+--LOAD ĐƠN HÀNG
+CREATE PROC USP_DonHangList @userName VARCHAR(30)
+AS 
+BEGIN
+	DECLARE @maKH INT
+	SET @maKH= (SELECT MAKH FROM TAIKHOAN WHERE @userName =username)
+	SELECT MAHD,TRANGTHAI, NGAYLAP, TRANGTHAITHANHTOAN, HINHTHUCTHANHTOAN FROM HOADON
+	WHERE @maKH = maKH
+END
+GO
+
+--LOAD CHI TIẾT ĐƠN HÀNG
+CREATE PROC USP_ChiTietDH @maDH INT
+AS
+BEGIN
+	SELECT maSP, soLuong, tongCong FROM CHITIEDONHANG WHERE MAHOADON=@maDH
+END
+GO
+
+--TONG TIEN CHI TIÊT
+CREATE PROC USP_TongTien @maDH INT
+AS
+BEGIN
+	SELECT SUM(TONGCONG) FROM CHITIEDONHANG WHERE MAHOADON=@maDH
+END
+GO
+
+--USP_THEMHOADONTHANHTOANTHE
+CREATE PROC USP_THEMHDTTT @maHD INT, @maThe bigint
+AS
+BEGIN
+	INSERT INTO dbo.HOADONTHANHTOANTHE
+	VALUES  (@maHD,@maThe,NULL)
+END
+GO
+
+--USP_THEMCHITIETDONHANG
+CREATE PROC ThemCTDH @maDH INT, @maKH INT
+AS
+    BEGIN
+        DECLARE @maSP INT,
+            @soLuong INT ,
+            @tong FLOAT
+        DECLARE cursorGH CURSOR
+        FOR
+            SELECT  MASP ,
+                    SOLUONG,
+					tongTien
+            FROM    GIOHANG
+            WHERE   MAKH = @maKH
+        OPEN cursorGH                
+
+        FETCH NEXT FROM cursorGH     
+		INTO @maSP, @soLuong,@tong
+
+        WHILE @@FETCH_STATUS = 0
+            BEGIN
+
+                INSERT  dbo.CHITIEDONHANG
+                VALUES  ( @maSP, -- MADH - INT
+                          @maDH,
+						  @soluong, -- MASP - INT
+                          @tong,
+						  0,
+						  NULL  -- TONGTAM - float
+                          )
+				
+                FETCH NEXT FROM cursorGH
+          INTO @maSP, @soLuong,@tong
+            END
+
+        CLOSE cursorGH              
+        DEALLOCATE cursorGH
+    END
+GO
+
+--USP_THEMDONHANG
+CREATE PROC USP_THEMDONHANG @userName VARCHAR(30) , @trangThai NVARCHAR(50) , @trangThaiThanhToan NVARCHAR(50) , @hinhThucThanhToan NVARCHAR(50) , @maThe BIGINT
+AS
+BEGIN
+	DECLARE @MAKH INT,
+			@MAHD INT,
+			@TONGCONG FLOAT
+	SET @maKH= (SELECT MAKH FROM TAIKHOAN WHERE @userName =username)
+	SET @MAHD = (SELECT MAX(MAHD) FROM HOADON)
+	IF (@MAHD IS NULL)
+		BEGIN
+			SET @MAHD=1
+		END
+	ELSE
+		BEGIN
+			SET @MAHD =@MAHD +1
+		END
+	SET @TONGCONG = (SELECT SUM(TONGTIEN)
+					FROM GIOHANG G
+					WHERE @MAKH = G.maKH)
+	INSERT INTO HOADON VALUES (@MAHD,@MAKH,@trangThai,@TONGCONG,GETDATE(),NULL,NULL,@trangThaiThanhToan,@hinhThucThanhToan)
+	EXEC dbo.ThemCTDH @maHD,@MAKH
+	IF(@maThe != 0)
+	BEGIN
+	EXEC dbo.USP_THEMHDTTT @maHD ,@maThe
+	END
+END
+GO
+--XÓA TOÀN BỘ GIỎ HÀNG
+CREATE PROC USP_XoaTatCaGioHang @userName VARCHAR(30)
+	AS
+	BEGIN
+		DECLARE @maKH INT
+		SET @maKH= (SELECT MAKH FROM dbo.TAIKHOAN WHERE @userName =username)
+		DELETE GIOHANG WHERE @maKH=MAKH
+	END
+GO
+--------------
+-------------------------------------------------------------------------------
+--UYÊN
+create procedure dt_TatCaDoiTac
+as 
+begin 
+select * from DOITAC
+end 
+go
+
+create procedure dt_TimDoiTac (@maDT int)
+as 
+begin 
+select * from DOITAC where maDT = @maDT
+end 
+go
+
+create procedure dt_ThemDoiTac (@maDt int, @tenDT nvarchar(50), @ngayKiHopDong date, @thoiHan date, @viTriDang nvarchar(50) )
+as 
+begin 
+insert into DOITAC values (@maDt,@tenDT,@ngayKiHopDong,@thoiHan,@viTriDang)
+end 
+go 
+
+create procedure sp_TatCaSP
+as
+begin 
+select * from SANPHAM
+end 
+go
+
+create procedure sp_TimSanPham (@maSP int)
+as 
+begin 
+select * from SANPHAM where maSP = @maSP
+end 
+go
+
+create procedure ttt_XemDS 
+as 
+begin 
+select * from DANHSACHTTT
+end 
+go
+
+create procedure ttt_ThemDS (@maSP int,@maDT int, @ngayKiHopDong date ,@ngayDangTin date,@hanDangTin date )
+as 
+begin 
+insert into DANHSACHTTT values (@maSP,@maDT,@ngayKiHopDong,@ngayDangTin,@hanDangTin)
+end 
+go
+
+-----------------------------------------------------
+--Vy
+
+CREATE PROC THONGTINHOADON @maHoaDon INT
+AS
+    BEGIN
+        SELECT  maHD AS N'Mã hóa đơn' ,
+                maKH AS N'Mã khách hàng' ,
+                trangThai AS N'Trạng thái đơn hàng' ,
+				tongCong AS N'Tổng cộng' ,
+                ngayLap AS N'Ngày lập' ,
+				maNVGiaoHang AS N'Mã NV GH' ,
+				trangThaiThanhToan AS N'Trạng thái thanh toán' ,
+                hinhThucThanhToan AS N'Hình thức thanh toán'
+        FROM    dbo.HOADON
+        WHERE   maHD = @maHoaDon
+    END
+GO 
+
+CREATE PROC CAPNHATTRANGTHAIDONHANG @maHoaDon INT, @trangThai NVARCHAR(50)
+AS
+    BEGIN
+        UPDATE dbo.HOADON
+		SET trangThai = @trangThai
+		WHERE maHD = @maHoaDon
+		UPDATE dbo.HOADON
+		SET trangThaiThanhToan = N'Thanh toán thành công'
+		WHERE maHD = @maHoaDon AND trangThai LIKE N'%Giao hàng thành công%' AND hinhThucThanhToan LIKE N'%Thanh toán tiền mặt%'
+    END
+
+CREATE PROC THONGTINHOADONTTTHE @maHoaDon INT
+AS
+    BEGIN
+        SELECT  hd.maHD AS N'Mã hóa đơn' ,
+                maKH AS N'Mã khách hàng' ,
+				tongCong AS N'Tổng cộng' ,
+                ngayLap AS N'Ngày lập' ,
+				trangThaiThanhToan AS N'Trạng thái thanh toán' ,
+				the.MATHE AS N'Mã thẻ',
+				the.XACNHAN AS N'Trạng thái xác nhận'
+        FROM    dbo.HOADON hd, dbo.HOADONTHANHTOANTHE the
+        WHERE   hd.maHD = @maHoaDon AND the.MAHD = hd.maHD
+    END
+GO
+CREATE PROC XACNHANTINHTRANGTHANHTOAN @maHoaDon INT, @trangThai NVARCHAR(50)
+AS
+    BEGIN
+        UPDATE dbo.HOADONTHANHTOANTHE
+		SET XACNHAN = @trangThai
+		WHERE maHD = @maHoaDon
+    END
+
+
+--Tuyền Nguyễn
+create procedure dt_TatCaNhanVien
+as 
+begin 
+select * from NHANVIEN
+end 
+go
+
+create procedure dt_NhapMaNV (@MaNV int)
+as 
+begin 
+select * from NHANVIEN where MaNV = @MaNV
+end 
+go
+
+create procedure sp_TatCaSP
+as
+begin 
+select * from SANPHAM
+end 
+go
+
+create procedure sp_TimSanPham (@maSP int)
+as 
+begin 
+select * from SANPHAM where maSP = @maSP
+end 
+go
+
+
